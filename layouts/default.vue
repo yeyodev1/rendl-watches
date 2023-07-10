077b6ceee0cc46cc6f696fe4c19e337b42171eb8
Diego Reyes <diegorele13@gmail.com>
2023-07-10 17:34:20 -0500

    positioned the header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="DefaultLayout">
+  <div class="app-container">
     <TheHeader />
-    <slot  />
+    <div class="app-container-view">
+      <slot  />
+    </div>
     <TheFooter  />
   </div>
 </template>
+
+<style lang="scss">
+.app-container {
+	min-height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	&-view{
+		flex: 1;
+	}
+}
+</style>
