841e270c80bf1f23b191292a7d549123eca87a56
Diego Reyes <diegorele13@gmail.com>
2023-07-10 17:34:03 -0500

    done header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,33 @@
 <template>
-  <div> headerrrrrrrr</div>
+  <header class="header">
+    <img 
+      class="header-logo"
+      src="@/assets/images/logo.png" 
+      alt="rendl importadora">
+      <div class="header-options">
+        <a href="/">Hombre</a>
+        <a href="/">Mujer</a>
+      </div>
+  </header>
 </template>
 
-<style scoped>
-div {
-  height: 100px;
-  background-color: aqua;
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &-logo {
+    height: 64px;
+    padding: 12px;
+  }
+  &-options {
+    display: flex;
+    gap: 12px;
+    padding: 12px;
+    a {
+      text-decoration: none;
+      color: $black;
+    }
+  }
 }
 </style>
