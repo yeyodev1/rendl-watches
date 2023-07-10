9a2c822332cd930e6237f40429695f3d1f75100f
Diego Reyes <diegorele13@gmail.com>
2023-07-10 16:52:07 -0500

    set: crush styles and fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
     '@': fileURLToPath(new URL('./', import.meta.url))
+  },
+  vite: {
+    plugins: [],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+            @import '@fortawesome/fontawesome-free/scss/solid.scss';
+            @import '@fortawesome/fontawesome-free/scss/regular.scss';
+            @import '@fortawesome/fontawesome-free/scss/brands.scss';
+            @import '@nabux-crush/crush-styles/nabuxInternalVariables';
+            @import '@/styles/styles.scss';
+          `
+        }
+      },
+    }
   }
 })
