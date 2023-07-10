8dece7a62a7d7c02522e099e462809c054ff072e
Diego Reyes <diegorele13@gmail.com>
2023-07-10 16:20:02 -0500

    set: footer and header

---
+++ components/TheFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer></footer>
+</template>
+
+<style scoped>
+footer {
+  height: 200px;
+  background-color: bisque;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div> headerrrrrrrr</div>
+</template>
+
+<style scoped>
+div {
+  height: 100px;
+  background-color: aqua;
+}
+</style>
