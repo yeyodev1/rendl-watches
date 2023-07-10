9b98e32f655c6c12e25739827eb08df5b77f6768
Diego Reyes <diegorele13@gmail.com>
2023-07-10 17:33:54 -0500

    set: styles

--- app.vue
+++ app.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
+  <div class="app-container">
     <NuxtLayout>
-      <NuxtPage  />
+      <div class="app-container-view">
+        <NuxtPage   />
+      </div>
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+
+.app-container {
+	min-height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	&-view{
+		flex: 1;
+	}
+}
+</style>
