3008daca0970b635544cc6e42d25eaea0b788b35
Diego Reyes <diegorele13@gmail.com>
2023-07-10 16:53:06 -0500

    set: pages

---
+++ pages/Contact.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>esta es la pagina de contacto</h2>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
-<script setup></script>
-
 <template>
+  <h1>Index page</h1>
   <div>
-    INDEX PAGE 
+    <p class="hola">
+      hola
+    </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+  & .hola {
+    font-size: 100px;
+  }
+}
+</style>
