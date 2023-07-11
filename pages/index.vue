0214788301220a904f2b51e406b60680c8220ac4
Diego Reyes <diegorele13@gmail.com>
2023-07-11 10:53:42 -0500

    set: product section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import ProductSection from './home/ProductSection.vue';
+import TheHero from './home/TheHero.vue';
+</script>
+
 <template>
-  <h1>Index page</h1>
+  <TheHero />
+  <ProductSection />
 </template>
