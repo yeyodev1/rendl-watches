665094b69139bd6c5e5a843c9f7ab640c6404610
Diego Reyes <diegorele13@gmail.com>
2023-07-11 15:51:05 -0500

    set new location

---
+++ components/home/ProductSection.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const products = [
+  {
+    name: 'Reloj ultraviolento',
+    price: '50',
+    image: 'https://i.pinimg.com/564x/38/40/f1/3840f1da0f70a97b2db4d6642f21a36a.jpg'
+  },
+  {
+    name: 'Reloj ultraviolento',
+    price: '50',
+    image: 'https://i.pinimg.com/564x/38/40/f1/3840f1da0f70a97b2db4d6642f21a36a.jpg'
+  },
+  {
+    name: 'Reloj ultraviolento',
+    price: '50',
+    image: 'https://i.pinimg.com/564x/38/40/f1/3840f1da0f70a97b2db4d6642f21a36a.jpg'
+  },
+  {
+    name: 'Reloj ultraviolento',
+    price: '50',
+    image: 'https://i.pinimg.com/564x/38/40/f1/3840f1da0f70a97b2db4d6642f21a36a.jpg'
+  },
+]
+</script>
+
+<template>
+  <div class="product">
+    <p class="product-section-title">Nuevos en la colección</p>
+    <div class="product-section">
+      <TheProduct
+        v-for="(product, index) in products"
+        :key="index"
+        :image="product.image"
+        :product="product.name" 
+        :price="product.price"/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.product {
+  padding: 40px;
+  gap: 40px;
+  display: flex;
+  flex-direction: column;
+  &-section {
+    margin: 0 auto;
+    max-width: $desktop-upper-breakpoint;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 40px;
+    justify-content: center;
+    align-items: center;
+    &-title {
+    text-align: center;
+    font-weight: 700;
+    font-size: $h2-font-size;
+    }
+  }
+}
+</style>
---
+++ components/home/ProductsInOfferSection.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const products = [
+  {
+    image: 'https://i.pinimg.com/236x/51/e4/e5/51e4e57298fb43f902f5c564b5b14646.jpg',
+    name: 'Relok de la muerte',
+    price: '100',
+    oldPrice: '500',
+  },
+  {
+    image: 'https://i.pinimg.com/236x/51/e4/e5/51e4e57298fb43f902f5c564b5b14646.jpg',
+    name: 'Relok de la muerte',
+    price: '100',
+    oldPrice: '500',
+  },
+  {
+    image: 'https://i.pinimg.com/236x/51/e4/e5/51e4e57298fb43f902f5c564b5b14646.jpg',
+    name: 'Relok de la muerte',
+    price: '100',
+    oldPrice: '500',
+  },
+  {
+    image: 'https://i.pinimg.com/236x/51/e4/e5/51e4e57298fb43f902f5c564b5b14646.jpg',
+    name: 'Relok de la muerte',
+    price: '100',
+    oldPrice: '500',
+  },
+]
+</script>
+
+<template>
+  <div class="section">
+    <p class="section-title">
+      Relojes en oferta
+    </p>
+    <div class="section-products">
+      <TheProductInOffer 
+        v-for="(product, index) in products"
+        :key="index"
+        :image="product.image"
+        :product="product.name"
+        :price="product.price" 
+        :oldPrice="product.oldPrice" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  padding: 40px;
+  gap: 40px;
+  display: flex;
+  flex-direction: column;
+  &-title {
+    text-align: center;
+    font-weight: 700;
+    font-size: $h2-font-size;
+  }
+  &-products {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 40px;
+    justify-content: center;
+  }
+}
+</style>
