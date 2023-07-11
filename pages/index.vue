e6504cb63a29a131481e1fcc5903ec948164cee7
Diego Reyes <diegorele13@gmail.com>
2023-07-10 23:06:48 -0500

    rm: style unnecesary

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,3 @@
 <template>
   <h1>Index page</h1>
-  <div>
-    <p class="hola">
-      hola
-    </p>
-  </div>
 </template>
-
-<style lang="scss" scoped>
-div {
-  & .hola {
-    font-size: 100px;
-  }
-}
-</style>
