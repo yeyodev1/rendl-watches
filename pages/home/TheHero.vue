ea51d5bae9bf7c2bad3d68895bad05cf15944cd9
Diego Reyes <diegorele13@gmail.com>
2023-07-11 10:54:41 -0500

    done: hero

---
+++ pages/home/TheHero.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hero">
+      <p>hola</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  width: 100%;
+  height: 40vh;
+  background-image: url('./assets/images/heroImage.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-position: top center;
+  padding: 48px 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+}
+</style>
