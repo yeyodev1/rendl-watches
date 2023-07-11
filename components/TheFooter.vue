52dbd4aa2d519a83542cdacbea07278e1fb58503
Diego Reyes <diegorele13@gmail.com>
2023-07-10 23:06:20 -0500

    done footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,10 +1,76 @@
+<script setup lang="ts">
+const networks = [
+  {
+    link: '/',
+    icon: 'fa-brands fa-facebook'
+  },
+  {
+    link: '/',
+    icon: 'fa-brands fa-instagram'
+  },
+  {
+    link: '/',
+    icon: 'fa-brands fa-whatsapp'
+  },
+]
+</script>
+
 <template>
-  <footer></footer>
+  <footer class="footer">
+    <img 
+      class="footer-image"
+      src="@/assets/images/logo.png" 
+      alt="rendl">
+    <div class="footer-options">
+      <a href="/">Hombre</a>
+      <a href="/">Mujer</a>
+    </div>
+    <div class="footer-social">
+      <a
+        v-for="(net, index) in networks"
+        :key="index"
+        :href="net.link">
+        <i :class="net.icon"/>
+      </a>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
-footer {
-  height: 200px;
-  background-color: bisque;
+<style lang="scss" scoped>
+.footer {
+  max-width: $desktop-upper-breakpoint;
+  padding: 12px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 48px 24px;
+  @media(min-width: $tablet-lower-breakpoint) {
+    justify-content: space-around;
+  }
+  &-image{
+    height: 64px;
+  }
+  &-options {
+    display: flex;
+    gap: 12px;
+    padding: 12px;
+    justify-content: center;
+    align-items: center;
+    a {
+      text-decoration: none;
+      color: $black;
+    }
+  }
+  &-social {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+    a i {
+      color: $black;
+      font-size: 24px;
+    }
+  }
 }
 </style>
