594d59f222950a327c432cc80a20e85554f6b9bc
Diego Reyes <diegorele13@gmail.com>
2023-07-11 13:43:47 -0500

    done call to action

---
+++ pages/home/TheCta.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="section">
+    <div class="section-option">
+      <p>Mujer</p>
+    </div>
+    <div>
+      <img src="@/assets/images/logo.png" alt="">
+    </div>
+    <div class="section-option">
+      <p>Hombre</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &-option {
+    padding: 80px 120px;
+  }
+}
+</style>
