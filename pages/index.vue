fbf7b68679006695069582a3c03169272cfb8a73
Diego Reyes <diegorele13@gmail.com>
2023-07-12 09:08:49 -0500

    set: hero image as a prop

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import TheHero from '@/components/home/TheHero.vue';
+import TheHero from '@/components/TheHero.vue';
 import ProductSection from '@/components/home/ProductSection.vue';
 import TheCta from '@/components/home/TheCta.vue';
 import ProductsInOfferSection from '@/components/home/ProductsInOfferSection.vue';
 </script>
 
 <template>
-  <TheHero />
+  <TheHero :image="'../assets/images/heroImage.png'"/>
   <ProductSection />
   <TheCta />
   <ProductsInOfferSection />
