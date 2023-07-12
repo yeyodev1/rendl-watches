559a61ab735d41179f8e3b253da4538f63839846
Diego Reyes <diegorele13@gmail.com>
2023-07-12 10:08:49 -0500

    working properly

--- pages/Watches.vue
+++ pages/Watches.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import TheHero from '@/components/TheHero.vue';
+import AllProducts from '@/components/watches/AllProducts.vue';
 </script>
 
 <template>
   <div class="container">
     <TheHero image="https://i.pinimg.com/564x/53/bd/f0/53bdf0f70c7253ef9fc60319750de117.jpg" description="Todos los relojes"/>
+    <AllProducts />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.container{
-  position: relative;
-  display: flex;
-  align-items: center;
-}
 </style>
