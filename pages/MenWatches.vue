e5a67de0f98833938cd1a7b5bdf9ba1e9769006d
Diego Reyes <diegorele13@gmail.com>
2023-07-12 10:29:12 -0500

    set: page of men products

---
+++ pages/MenWatches.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheHero from '@/components/TheHero.vue';
+import MenProducts from '@/components/watches/MenProducts.vue';
+</script>
+
+<template>
+  <TheHero 
+  image="https://i.pinimg.com/236x/43/76/7c/43767c8df6eb4870409ae00e96585f0c.jpg" 
+  description="Relojes para hombre"/>
+  <MenProducts />
+</template>
+
+<script lang="scss" scoped ></script>
