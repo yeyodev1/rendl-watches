066643d90c9bb63762642614da18c45281b6371c
Diego Reyes <diegorele13@gmail.com>
2023-07-12 09:08:24 -0500

    relocate and made this a componente

---
+++ components/TheHero.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  image: {
+    type: String, 
+    requiered: true,
+  }
+})
+</script>
+
+<template>
+  <div class="hero">
+      <img class="hero-image" :src="image" alt="Encuentra tu reloj aquí">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  width: 100%;
+  height: 40vh;
+  &-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
