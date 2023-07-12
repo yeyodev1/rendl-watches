63f3f97b017d6836112d8f4c6cccf4b14bd73e8b
Diego Reyes <diegorele13@gmail.com>
2023-07-12 10:19:23 -0500

    done woman products page

---
+++ pages/WomanWatches.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheHero from '@/components/TheHero.vue';
+import WomanProducts from '@/components/watches/WomanProducts.vue'
+</script>
+
+<template>
+  <TheHero  
+    image="https://i.pinimg.com/564x/fd/50/01/fd5001cd7076d0b889d6d376eff1afbc.jpg" 
+    description="Relojes de mujer"/>
+  <WomanProducts />
+</template>
+
+<style lang="scss" scoped></style>
