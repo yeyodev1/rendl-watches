fd5426d81ece0d13d373b77d70233f18ced66be1
Luis Reyes <lualreye@gmail.com>
2023-07-16 20:56:33 -0500

    fix: image import

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,11 @@ import TheHero from '@/components/TheHero.vue';
 import ProductSection from '@/components/home/ProductSection.vue';
 import TheCta from '@/components/home/TheCta.vue';
 import ProductsInOfferSection from '@/components/home/ProductsInOfferSection.vue';
+import HeroImage from '@/assets/images/heroImage.png'
 </script>
 
 <template>
-  <TheHero :image="'../assets/images/heroImage.png'"/>
+  <TheHero :image="HeroImage"/>
   <ProductSection />
   <TheCta />
   <ProductsInOfferSection />
