21c2eea1aa49cfbd06a01133b42001b016500791
Diego Reyes <diegorele13@gmail.com>
2023-07-17 10:53:48 -0500

    set: all products component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import TheHero from '@/components/TheHero.vue';
-import ProductSection from '@/components/home/ProductSection.vue';
 import TheCta from '@/components/home/TheCta.vue';
 import ProductsInOfferSection from '@/components/home/ProductsInOfferSection.vue';
 import HeroImage from '@/assets/images/heroImage.png'
+import AllProducts from '@/components/watches/AllProducts.vue';
 </script>
 
 <template>
   <TheHero :image="HeroImage"/>
-  <ProductSection />
+  <AllProducts title="Nuevos en la colección"/>
   <TheCta />
   <ProductsInOfferSection />
 </template>
