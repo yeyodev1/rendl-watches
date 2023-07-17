75b7f37eecc629a6045d4ca25f3b6bbd50f04e43
Diego Reyes <diegorele13@gmail.com>
2023-07-17 18:20:44 -0500

    set: hero

--- pages/WomanWatches.vue
+++ pages/WomanWatches.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import TheHero from '@/components/TheHero.vue';
 import WomanProducts from '@/components/watches/WomanProducts.vue'
+import heroImage from '@/assets/images/heroWoman.png';
 </script>
 
 <template>
   <TheHero  
-    image="https://i.pinimg.com/564x/fd/50/01/fd5001cd7076d0b889d6d376eff1afbc.jpg" 
+    :image="heroImage" 
     description="Relojes de mujer"/>
   <WomanProducts />
 </template>
