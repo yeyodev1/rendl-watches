940f4dc3e71548d5458bf2903d945791ba2eeff9
Diego Reyes <diegorele13@gmail.com>
2023-07-17 18:09:55 -0500

    set: header to mobile devices

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import useMenuStore from '@/store/MenuStore';
+
+const menuStore = useMenuStore();
+
+function toggleMenu() {
+  menuStore.toggleMenu();
+}
+</script>
+
 <template>
   <header class="header">
     <NuxtLink to="/">
@@ -6,6 +16,11 @@
         src="@/assets/images/logo.png" 
         alt="rendl importadora" />
     </NuxtLink>
+      <button 
+        class="header-menu"
+        @click="toggleMenu">
+        <i class="fa-solid fa-bars" />
+      </button>
       <div class="header-options">
         <NuxtLink to="/menwatches">Hombre</NuxtLink>
         <NuxtLink to="/womanwatches">Mujer</NuxtLink>
@@ -25,10 +40,22 @@
     height: 64px;
     padding: 12px;
   }
-  &-options {
-    display: flex;
-    gap: 12px;
+  &-menu {
     padding: 12px;
+    background: none;
+    border: none;
+    text-decoration: none;
+    @media(min-width: $tablet-upper-breakpoint) {
+      display: none;
+    }
+  }
+  &-options {
+    display: none;
+    @media(min-width: $tablet-upper-breakpoint) {
+      display: flex;
+      gap: 12px;
+      padding: 12px;
+    }
     * {
       text-decoration: none;
       color: $black;
