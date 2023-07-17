9a39a0429666cbe23b99c8f79357014dbd4bbb07
Diego Reyes <diegorele13@gmail.com>
2023-07-17 15:48:41 -0500

    details are working properly

---
+++ components/DetailProduct.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = defineProps({
+  brand: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  material: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <li
+    class="detail"
+  >
+    {{ brand }}
+  </li>
+  <li class="detail">
+    {{ type }}
+  </li>
+  <li class="detail">
+    {{ material }} 
+  </li>
+  <li class="detail">
+    {{ code }} 
+  </li>
+</template>
+  
+<style lang="scss" scoped>
+  .detail {
+      color: $gray;
+      margin-top: 8px;
+    }
+</style>
