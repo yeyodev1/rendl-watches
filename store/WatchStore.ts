6991aa9a7c21a61698eb9190ed6bb9332f0055e2
Diego Reyes <diegorele13@gmail.com>
2023-07-17 13:38:26 -0500

    set: enum

--- store/WatchStore.ts
+++ store/WatchStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 import WatchesService from '../api/WatchesService';
+import { Watch } from '@/enum/Watch';
 
 const watchesService = new WatchesService();
 
@@ -8,17 +9,19 @@ const useWatchStore = defineStore('useWatchStore', {
     manWatches: [],
     womanWatches: [],
     allWatches: [],
+    offerWatches: [],
   }),
   actions: {
     async getWatches(gender: string) {
       try {
         const products = await watchesService.getWatches(gender);
-        if (gender === 'men') {
+        if (gender === Watch.MEN) {
           this.manWatches = products.data.stories;
-        } else if (gender === 'woman') {
+        } else if (gender === Watch.WOMEN) {
           this.womanWatches = products.data.stories;
         }
-        this.allWatches =  [...this.manWatches, ...this.womanWatches];
+        this.allWatches = [...this.manWatches, ...this.womanWatches];
+        this.offerWatches = this.allWatches.filter(watch => watch.content.old_price)
       } catch (error) {
         console.error(error);
       }
